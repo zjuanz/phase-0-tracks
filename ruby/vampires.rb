368e39c718a2fe31f21550d6583e168af31a0766
Juan L. Zalles <juan.zalles@gmail.com>
2017-02-15 14:28:51 -0600

    4.4 rel 1 questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "What year were you born?"
+birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance"
+insurance = gets.chomp
+
+p name, age, birth_year, garlic, insurance
