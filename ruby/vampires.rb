bff33d1f98e8a1d14cf906cbbec42546d7b95d53
Juan L. Zalles <juan.zalles@gmail.com>
2017-02-16 06:23:54 -0600

    4.4 release 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,13 +1,47 @@
+puts "How many employees are there ?"
+employees=gets.chomp.to_i
+for i in 1..employees do
+  puts "What is your name?"
+  name = gets.chomp
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  puts "What year were you born?"
+  birth_year = gets.chomp.to_i
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?(y/n)"
+  garlic = gets.chomp
+  puts "Would you like to enroll in the company's health insurance?(y/n)"
+  insurance = gets.chomp
 
-puts "What is your name?"
-name = gets.chomp
-puts "How old are you?"
-age = gets.chomp
-puts "What year were you born?"
-birth_year = gets.chomp
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
-puts "Would you like to enroll in the company's health insurance"
-insurance = gets.chomp
+  p name, age, birth_year, garlic, insurance
 
-p name, age, birth_year, garlic, insurance
+
+  probably_not_a_vampire=false;
+  if ((2017-birth_year-age).abs<=1) && ((garlic=="y") || (insurance=="y"))
+    probably_not_a_vampire=true;
+  end
+  probably_a_vampire=false
+  if ((2017-birth_year-age).abs>1) && ((garlic=="n") || (insurance=="n"))
+    probably_a_vampire=true
+  end
+  almost_certainly_a_vampire=false
+  if (((2017-birth_year-age).abs>1) && (garlic=="n") && (insurance=="n"))
+    almost_certainly_a_vampire=true
+  end
+  definitely_a_vampire=false
+  if name=="Drake Cula" || name=="Tu Fang"
+    definitely_a_vampire=true
+  end
+
+  case 
+  when definitely_a_vampire
+    puts "Definitely a vampire"
+  when almost_certainly_a_vampire
+    puts "Almost certainly a vampire"
+  when probably_a_vampire
+    puts "Probably a vampire"
+  when probably_not_a_vampire
+    puts "Probably not a vampire"
+  else
+    puts "Results inconclusive"
+  end
+end   
