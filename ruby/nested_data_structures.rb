c3539634434d802d0aa0b5f8620a60ec4c83fcf0
Juan L. Zalles <juan.zalles@gmail.com>
2017-02-24 05:56:15 -0600

    added methods to print todos

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,6 +2,7 @@
 # nested data structure exercise
 #
 homestead = {
+  pretty_name: "Zalles/Bourianoff Homestead",
   house: {
     pretty_name: 'House',
     to_dos: [
@@ -60,3 +61,26 @@ homestead = {
   }
 }
 # p homestead
+def print_todo (td)
+#  p "td==>",td
+  print "\tTodo: ",td[:owner],"==>",td[:task],"\n"
+end
+def print_todos (tds)
+#  p "tds==>",tds
+  if tds!=nil 
+    then tds.each {|td| print_todo(td)}
+  end
+end
+def print_container (c)
+#  p "c==>",c
+  if c[:to_dos] != nil
+  then 
+    print c[:pretty_name]," has the following todos:\n"
+    print_todos c[:to_dos]
+  else print c[:pretty_name]," has no todos\n"
+  end
+end
+print_todos(homestead[:to_dos])
+print_container(homestead)
+print_container(homestead[:house])
+print_container(homestead[:house][:kitchen])
