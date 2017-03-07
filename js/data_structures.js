237db392b7396799c3debc24c4a3ef4a5c661444
Juan L. Zalles <juan.zalles@gmail.com>
2017-03-07 13:05:18 -0600

    7.2 data structures release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -23,3 +23,21 @@ colors.push("pink");
 names.push("Trump");
 
 console.log(names,colors);
+
+//
+// release 1
+// 
+//  create object with name->color 
+//
+
+var horses = {};
+
+//
+// loop through arrays to populate horses
+//
+
+for (var i = 0; i <names.length; i++) {
+  horses[names[i]]=colors[i];
+}
+
+console.log(horses);
