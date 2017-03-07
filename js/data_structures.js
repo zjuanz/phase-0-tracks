c5db7e09c0a2d5347b06d0d7cbac51c717491223
Juan L. Zalles <juan.zalles@gmail.com>
2017-03-07 13:30:00 -0600

    7.2 data structures release 2

--- js/data_structures.js
+++ js/data_structures.js
@@ -41,3 +41,44 @@ for (var i = 0; i <names.length; i++) {
 }
 
 console.log(horses);
+
+//
+// release 2
+//
+
+// 
+// constructor function for a Car
+//  properties
+//    color
+//    make
+//    model
+//    year
+//
+//    honk function 
+//    start_engine
+//
+
+function Car(color,make,model,year) {
+
+  this.color = color;
+  this.make = make;
+  this.model = model;
+  this.year = year;
+
+  this.honk = function() { console.log("BEEEP!");};
+
+}
+
+var anotherCar = new Car("blue", 
+                          "BMW",
+                          "740",
+                          2016);
+console.log(anotherCar);
+anotherCar.honk();
+
+var yetAnotherCar = new Car("purple",
+                            "Mercedes Benz",
+                            "S-series",
+                            2017);
+console.log(yetAnotherCar);
+yetAnotherCar.honk();
