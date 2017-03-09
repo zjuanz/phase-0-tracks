5bad1d28a9412750e8c9b4cd0353ca7ece95b77d
Juan L. Zalles <juan.zalles@gmail.com>
2017-03-09 10:00:12 -0600

    7.3 solo release 2

--- js/algos.js
+++ js/algos.js
@@ -44,6 +44,42 @@ function share_key_value(x,y){
   return false; // no match found
 }
 
+function random_words(length) {
+  //
+  // build an array with length random words
+  //
+  // - words don't have to be real words
+  // - words are of random length min 1, max 10
+  // - random number algorithm from google search for random
+  //      Math.floor((Math.random() * 10) + 1);
+  // - use random to pull letters from alphabet string
+  //
+
+  var result=[];
+  var word_length=0;
+
+//  console.log("array length",length);
+
+  for (var i = 0; i < length; i++) {
+    // build up each word of the array
+    word_length=Math.floor((Math.random()*10)+1);
+    result[i]=random_word(word_length);
+  }
+//  console.log(result,result.length);
+  return result;
+}
+function random_word(length) {
+  var alphabet="abcdefghijklmnopqrstuvwxyz";
+  result="";
+  for (var i = 0; i < length; i++) {
+  
+    alphabet_random=Math.floor((Math.random()*26));
+    result+=alphabet.charAt(alphabet_random);
+  }
+//  console.log(length,result);
+  return result;
+}
+
 console.log(longest(test_array));
 
 test_array = ["a","aa","aaa",
@@ -70,4 +106,28 @@ test_object_2 = {
 test_object_3 = {}
 
 console.log(share_key_value(test_object,test_object_2));
+//
+// check to see how method handles empty object and failure
+//
 console.log(share_key_value(test_object,test_object_3));
+
+random_words(5);
+random_words(15);
+
+// 
+// do the following 10 times
+//    generate array
+//    print array
+//    get longest word and print
+//
+
+var array_length=0;
+var words=[];
+
+for (var i = 0; i < 10; i++) {
+  array_length=Math.floor((Math.random()*10)+1);
+  console.log("\n array of length:",array_length);
+  words=random_words(array_length);
+  console.log(words);
+  console.log("longest word:",longest(words));
+}
