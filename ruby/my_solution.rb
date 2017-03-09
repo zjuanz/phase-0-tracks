720a1a8e3162377685f2c0b295d91889a6106451
Juan L. Zalles <juan.zalles@gmail.com>
2017-03-08 22:10:28 -0600

    GPS 3.1 release 7

--- ruby/my_solution.rb
+++ ruby/my_solution.rb
@@ -55,18 +55,31 @@ class VirusPredictor
 #  def predicted_deaths(population_density, population, state)
   def predicted_deaths()
     # predicted deaths is solely based on population density
+    # if @population_density >= 200
+    #   number_of_deaths = (@population * 0.4).floor
+    # elsif @population_density >= 150
+    #   number_of_deaths = (@population * 0.3).floor
+    # elsif @population_density >= 100
+    #   number_of_deaths = (@population * 0.2).floor
+    # elsif @population_density >= 50
+    #   number_of_deaths = (@population * 0.1).floor
+    # else
+    #   number_of_deaths = (@population * 0.05).floor
+    # end
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      mult=0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      mult=0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      mult=0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      mult=0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      mult=0.05
     end
 
+    number_of_deaths = (@population * mult).floor 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
@@ -80,18 +93,18 @@ class VirusPredictor
   def speed_of_spread()
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
+    #speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
